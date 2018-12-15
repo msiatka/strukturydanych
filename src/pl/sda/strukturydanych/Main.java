f67569a57cabaeffa667f529b1fd028c879d1687
Marcin Siatka <siatka131@gmail.com>
2018-12-15 15:48:11 +0100

    Merge branch 'feature/sda'
    # Conflicts:
    #	.idea/workspace.xml
    #	out/production/struktury_danych/pl/sda/strukturydanych/Main.class
    #	src/pl/sda/strukturydanych/Main.java

--- src/pl/sda/strukturydanych/Main.java
+++ src/pl/sda/strukturydanych/Main.java
@@ -7,6 +7,7 @@ import pl.sda.strukturydanych.kolejka.KolejkaList;
 import pl.sda.strukturydanych.stos.StosList;
 import pl.sda.strukturydanych.stos.StosListIndexOutException;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -34,13 +35,144 @@ public class Main {
         //lista dwukierunkowa
         //wywolajLista();
 
-        wywolajZM();
+//        wywolajZM();
+//
+//        int tab[] = new int[10];
+//        tab[0] = 3;
+//        tab[1] = 4;
+//        tab[2] = 5;
+//        tab[3] = 6;
+//        tab[4] = 7;
+//        tab[5] = 9;
+//        tab[6] = 40;
+//        tab[7] = 45;
+//        tab[8] = 48;
+//        tab[9] = 78;
 
+//        System.out.println(" Czy tablica jest posortowana rosnąco ? -> " + testTabSort(tab));
+//        utworzTabele();
+//        System.out.println("------------");
+
+//        printTab(sumaEltablic(utworzTabele(),utworzTabele()));
+//
+//        printTab(sumaEltablicNierow(utworzTabele(),utworzTabele()));
+
+        System.out.println(" czy pierwsza: " + czyPierwsza());
+    }
+
+    public static boolean czyPierwsza(){
+        Boolean czyPierwsza = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        int i=0;
+        int j=1;
+        String liczba = scanner.next();
+        try{ int x = Integer.parseInt(liczba);
+            for(j=1;j<x+1;j++){
+                if (x % j == 0) {
+                    i++;
+                }
+            }
+            if(i>2){ czyPierwsza = false; }
+
+            if(i==2){czyPierwsza = true;}
+
+        }catch (IllegalArgumentException e) {
+            System.out.println(" LIPA");
+        }
+        return czyPierwsza;
     }
 
     public static void wywolajZM() {
 
         System.out.println(" koemntarz");
+    }
+
+    public static boolean testTabSort(int tab[]){
+
+        for(int i=0;i<(tab.length-1);i++){
+            if(!(tab[i]<tab[i+1])){return false;}
+        }
+        return true;
+
+    }
+
+    public static void printTab(int[] tab){
+        System.out.println();
+        System.out.println("++++++++++++++++++++");
+        System.out.println(" ");
+        for(int j=0;j<tab.length;j++){
+            System.out.print(tab[j]+ " ");
+        }
+    }
+
+    public static int[] utworzTabele() {
+        int licznik=0;
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj wymiar tablicy: ");
+        int size = scanner.nextInt();
+        int[] tab = new int[size];
+
+//        System.out.println("Podaj elementy tablicy:");
+//
+//        while(licznik!=size){
+//            tab[licznik]=scanner.nextInt();
+//            licznik++;
+//        }
+//
+//        System.out.println(" ");
+//        for(int j=0;j<tab.length;j++){
+//            System.out.print(tab[j]+ " ");
+//        }
+
+        int[] tab2 = new int[size];
+        Random generator = new Random();
+
+        System.out.println(" ");
+        for(int j=0;j<tab2.length;j++){
+           tab2[j]=generator.nextInt(100);
+        }
+
+        for(int j=0;j<tab2.length;j++){
+            System.out.print(tab2[j]+ " ");
+        }
+
+        return tab2;
+    }
+
+    public static int[] sumaEltablic(int[] x, int[] y){
+        int[] tab = new int[x.length];
+        if(x.length==y.length){
+            for(int i=0;i<x.length;i++){
+                tab[i]=x[i]+y[i];
+            }
+        }else {
+            System.out.println(" wymiary podanych tablic nie są równe - nie mozna ich zsumowac");
+            tab = null;
+            System.out.println(" NULL");
+        }
+
+        return tab;
+    }
+
+    public static int[] sumaEltablicNierow(int[] x, int[] y){
+        int[] tab;
+        int wym;
+
+        if(x.length<y.length) {
+           tab = new int[x.length];
+           wym = x.length;
+        } else {
+           tab = new int[y.length];
+            wym = y.length;
+        }
+
+        for(int i=0;i<wym;i++) {
+            tab[i] = x[i] + y[i];
+        }
+
+        return tab;
     }
 
     public static void wywolajLista(){
